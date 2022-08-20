dabd50c31c106df1a633d75f0ba791d84af5b7f4
Rafal Nojek <dornier1997@gmail.com>
2022-08-20 16:14:39 +0200

    Add notebook with basic Machine Learning models

--- src/features/Preprocessor.py
+++ src/features/Preprocessor.py
@@ -5,31 +5,27 @@ from dataclasses import dataclass
 from stop_words import get_stop_words
 
 
-@dataclass
 class Preprocessor:
-    """ 
+    """
     Class responsible for preprocessing text.
     """
-    language: str = 'pl'
-    stopwords_file_path:str  = 'polish_stopwords.txt'
-    
-    
 
     # need improvement to be more flexible (e.g. preprocess more than one language)
-    def __post_init__(self):
-        if self.language == 'pl':
+    def __init__(self, path_to_stopwords, language: str = "pl"):
+        self.language = language
+        if self.language == "pl":
             # load Morfeusz
-            self.morfeusz = morfeusz2.Morfeusz()
-            
+            self.morfeusz = morfeusz2.Morfeusz
+            self.stopwords_file_path: str = path_to_stopwords
             # load stopwords from nltk
-            self.stopwords = set(get_stop_words('polish'))
-            
+            self.stopwords = set(get_stop_words("polish"))
+
             # load stopwords from txt and union with stopwords
-            self.stopwords |= set(open(self.stopwords_file_path, 'r', encoding='utf-8').read().splitlines())
-            
-
-
-            
+            self.stopwords |= set(
+                open(self.stopwords_file_path, "r", encoding="utf-8")
+                .read()
+                .splitlines()
+            )
 
     # need to be implemented better (e.g. load from path)
     def fit(self, dataset: pd.DataFrame):
@@ -39,55 +35,57 @@ class Preprocessor:
             dataset: dataset to load
         """
         self.dataset = dataset
-    
+
     # TODO: add more preprocessing methods
     # preprocessing methods
-    def lowercase(self, cols:list):
+    def lowercase(self, cols: list):
         """
         Lowercases all words in dataset by given columns.
         Args:
             cols: list of columns to lowercase
         """
         if self.dataset is None:
-            raise Exception('Dataset is not loaded.')
+            raise Exception("Dataset is not loaded.")
 
         for col in cols:
             self.dataset[col] = self.dataset[col].str.lower()
-        
-        
-    def remove_punctuation(self, cols:list):
+
+    def remove_punctuation(self, cols: list):
         """
         Removes punctuation from dataset by given columns.
         Args:
             cols: list of columns to remove punctuation
         """
         if self.dataset is None:
-            raise Exception('Dataset is not loaded.')
+            raise Exception("Dataset is not loaded.")
 
         for col in cols:
-            self.dataset[col] = self.dataset[col].str.translate(str.maketrans('', '', string.punctuation + '„”“”’‘'))
-    
-    def remove_stopwords(self, cols:list):
+            self.dataset[col] = self.dataset[col].str.translate(
+                str.maketrans("", "", string.punctuation + "„”“”’‘")
+            )
+
+    def remove_stopwords(self, cols: list):
         """
         Removes stopwords from dataset by given columns.
         Args:
             cols: list of columns to remove stopwords
         """
         if self.dataset is None:
-            raise Exception('Dataset is not loaded.')
+            raise Exception("Dataset is not loaded.")
 
         for col in cols:
             # self.dataset[col] = self.dataset[col].apply(lambda x: ' '.join([word for word in x.split() if word not in self.stopwords]))
-            # self.dataset[col] = self.dataset[col].apply(lambda x: ' '.join([word for word in x.split() 
-            #                                                             if word not isinstance(word,None) 
+            # self.dataset[col] = self.dataset[col].apply(lambda x: ' '.join([word for word in x.split()
+            #                                                             if word not isinstance(word,None)
             #                                                             and word not in self.stopwords]))
 
-            self.dataset[col] = self.dataset[col].apply(lambda x: ' '.join([word for word in x.split()
-                                                                            if word not in self.stopwords]))
+            self.dataset[col] = self.dataset[col].apply(
+                lambda x: " ".join(
+                    [word for word in x.split() if word not in self.stopwords]
+                )
+            )
 
-            
-    
-    def lemmatize(self, cols:list):
+    def lemmatize(self, cols: list):
         """
         Lemmatizes dataset by given columns.
         Args:
@@ -105,107 +103,131 @@ class Preprocessor:
             list_of_words = text.split()
             lemmatized_list_of_words = []
 
-            for word in list_of_words:    
-                _, _, interpretation = self.morfeusz.analyse(word)[0]
+            for word in list_of_words:
+                _, _, interpretation = self.morfeusz().analyse(word)[0]
                 lemWord = interpretation[1]
-                lemWordStripped = lemWord.split(':', 1)[0].lower()
+                lemWordStripped = lemWord.split(":", 1)[0].lower()
                 lemmatized_list_of_words.append(lemWordStripped)
 
-            lemmatizedText = ' '.join(lemmatized_list_of_words)
+            lemmatizedText = " ".join(lemmatized_list_of_words)
 
             return lemmatizedText
-            
 
         if self.dataset is None:
-            raise Exception('Dataset is not loaded.')
+            raise Exception("Dataset is not loaded.")
 
         for col in cols:
             self.dataset[col] = self.dataset[col].apply(lambda x: pl_text_lemmatizer(x))
-            
-    
-    def remove_hashtags(self, cols:list):
+
+    def remove_hashtags(self, cols: list):
         """
         Removes hashtags from dataset by given columns.
         Args:
             cols: list of columns to remove hashtags
         """
         if self.dataset is None:
-            raise Exception('Dataset is not loaded.')
+            raise Exception("Dataset is not loaded.")
 
         for col in cols:
             # both implementations seem to work, haven't checked which one is faster
-            self.dataset[col] = self.dataset[col].apply(lambda x: ' '.join([word for word in x.split() if not word.startswith('#')]))
+            self.dataset[col] = self.dataset[col].apply(
+                lambda x: " ".join(
+                    [word for word in x.split() if not word.startswith("#")]
+                )
+            )
             # self.dataset[col] = self.dataset[col].apply(lambda x: re.sub(r'#[a-zA-Z0-9_]+', '', x))
-    
-    def remove_mentions(self, cols:list):
+
+    def remove_mentions(self, cols: list):
         """
         Removes mentions from dataset by given columns.
         Args:
             cols: list of columns to remove mentions
         """
         if self.dataset is None:
-            raise Exception('Dataset is not loaded.')
+            raise Exception("Dataset is not loaded.")
 
         for col in cols:
-            self.dataset[col] = self.dataset[col].apply(lambda x: ' '.join([word for word in x.split() if not word.startswith('@')]))
-        
-    def remove_urls(self, cols:list):
+            self.dataset[col] = self.dataset[col].apply(
+                lambda x: " ".join(
+                    [word for word in x.split() if not word.startswith("@")]
+                )
+            )
+
+    def remove_urls(self, cols: list):
         """
         Removes urls from dataset by given columns.
         Args:
             cols: list of columns to remove urls
         """
         if self.dataset is None:
-            raise Exception('Dataset is not loaded.')
+            raise Exception("Dataset is not loaded.")
 
         for col in cols:
-            self.dataset[col] = self.dataset[col].apply(lambda x: ' '.join([word for word in x.split() if not word.startswith('http') and not word.startswith('www.')]))
+            self.dataset[col] = self.dataset[col].apply(
+                lambda x: " ".join(
+                    [
+                        word
+                        for word in x.split()
+                        if not word.startswith("http") and not word.startswith("www.")
+                    ]
+                )
+            )
 
-
-    def remove_numbers(self, cols:list):
+    def remove_numbers(self, cols: list):
         """
         Removes numbers from dataset by given columns.
         Args:
             cols: list of columns to remove numbers
         """
         if self.dataset is None:
-            raise Exception('Dataset is not loaded.')
+            raise Exception("Dataset is not loaded.")
 
         for col in cols:
-            self.dataset[col] = self.dataset[col].apply(lambda x: ' '.join([word for word in x.split() if not word.isdigit()]))
-    
+            self.dataset[col] = self.dataset[col].apply(
+                lambda x: " ".join([word for word in x.split() if not word.isdigit()])
+            )
 
     # TODO: add more preprocessing methods
     #       check remove_emojis
-    def remove_emojis(self, cols:list):
+    def remove_emojis(self, cols: list):
         """
         Removes emojis from dataset by given columns.
         Args:
             cols: list of columns to remove emojis
         """
         if self.dataset is None:
-            raise Exception('Dataset is not loaded.')
+            raise Exception("Dataset is not loaded.")
 
         for col in cols:
-            self.dataset[col] = self.dataset[col].apply(lambda x: ' '.join([word for word in x.split() if not word.startswith(':')]))
-    
+            self.dataset[col] = self.dataset[col].apply(
+                lambda x: " ".join(
+                    [word for word in x.split() if not word.startswith(":")]
+                )
+            )
 
-    def transform(self, cols:list,lowercase=True,remove_punctuation=True,
-                                    remove_stopwords=True,lemmatize=True,
-                                    remove_hashtags=True,remove_urls=True,
-                                    remove_numbers=True,remove_emojis=True,
-                                    remove_mentions=True):
+    def transform(
+        self,
+        cols: list,
+        lowercase=True,
+        remove_punctuation=True,
+        remove_stopwords=True,
+        lemmatize=True,
+        remove_hashtags=True,
+        remove_urls=True,
+        remove_numbers=True,
+        remove_emojis=True,
+        remove_mentions=True,
+    ):
         """
         Preprocess dataset by given columns.
         Args:
             cols: list of columns to transform
-        
+
         Returns:
             transformed dataset
         """
         if self.dataset is None:
-            raise Exception('Dataset is not loaded.')
-
+            raise Exception("Dataset is not loaded.")
 
         self.lowercase(cols) if lowercase else None
         self.remove_mentions(cols) if remove_mentions else None
@@ -218,7 +240,7 @@ class Preprocessor:
         self.remove_stopwords(cols) if remove_stopwords else None
 
         return self.dataset
-    
+
     def fit_transform(self, dataset: pd.DataFrame):
         """
         Loads dataset from file and preprocesses it.
